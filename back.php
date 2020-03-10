ce3d5c0dc5063303b29ed1dece957ee4c63df579
Edward <olooedwards69@gmail.com>
2020-03-10 20:05:35 +0300

    updates

--- back.php
+++
@@ -1,5 +0,0 @@
-<video muted autoplay loop playsinline>
-        <source
-          src="https://player.vimeo.com/external/136078589.mobile.mp4?s=57b1ccad7f3bb9d7723692af6185673658a02589&profile_id=116"
-          type="video/mp4" />
-      </video>
